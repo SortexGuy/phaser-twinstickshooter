315c03321836ffa88e9917dcb5d51f1508b17635
Rubén A <sortexjuegos@gmail.com>
2024-04-17 20:41:13 -0400

    feat(server): Better comms with client

--- app/server/src/rooms/schema/ArenaState.ts
+++ app/server/src/rooms/schema/ArenaState.ts
@@ -1,16 +1,13 @@
 import { Schema, MapSchema, type } from '@colyseus/schema';
-
-// export class MyRoomState extends Schema {
-// 	@type('string') mySynchronizedProperty: string = 'Hello world';
-// }
+import { IArenaState, IPlayer } from '../../../../client/src/types/IArenaState'
 
 // An abstract player object, demonstrating a potential 2D world position
-export class Player extends Schema {
+export class Player extends Schema implements IPlayer {
 	@type('number') x: number = 0.0;
 	@type('number') y: number = 0.0;
 }
 
 // Our custom game state, an ArraySchema of type Player only at the moment
-export class ArenaState extends Schema {
+export class ArenaState extends Schema implements IArenaState {
 	@type({ map: Player }) players = new MapSchema<Player>();
 }
